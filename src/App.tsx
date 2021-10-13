cc48e9d2ee322e004d962b9b3193316ae7984d56
Nikita Tolstikov <web.tolst9k@gmail.com>
2021-10-13 10:40:47 +0300

    Add total hours component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Row } from 'antd';
 
 import { TaskInput } from './features/taskInput/TaskInput';
+import { TotalHours } from './features/totalHours/TotalHours';
 import './App.css';
 
 function App() {
   return (
     <Row justify="center" gutter={16} style={{ marginTop: '50px' }}>
         <TaskInput />
+        <TotalHours />
     </Row>
   );
 }
---
+++ src/features/totalHours/TotalHours.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Col } from 'antd';
+
+export function TotalHours(): JSX.Element {
+  // const selectedMonth = useAppSelector(selectSelectedMonth);
+  // const hoursWorkedInMonth = useAppSelector(selectHoursWorkedInMonth);
+
+  return (
+    <Col>
+      
+    </Col>
+  );
+}

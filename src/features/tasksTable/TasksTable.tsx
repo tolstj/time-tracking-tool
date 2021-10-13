9dec582f5461beac926d37b4590689b44d5aea53
Nikita Tolstikov <web.tolst9k@gmail.com>
2021-10-14 02:31:46 +0300

    Add selector of tasks of selected week

--- src/features/tasksTable/TasksTable.tsx
+++ src/features/tasksTable/TasksTable.tsx
@@ -4,9 +4,11 @@ import { Table } from 'antd';
 import { useAppSelector } from '../../app/hooks';
 import { selectSelectedWeekPeriod } from '../weekSwitcher/weekSwitcher.slice';
 import { getTableColumnsFromWeekPeriod } from './utils/table';
+import { selectSelectedWeekPeriodTasks } from './tasksTable.slice';
 
 export function TasksTable(): JSX.Element {
   const selectedWeekPeriod = useAppSelector(selectSelectedWeekPeriod);
+  const tasks = useAppSelector(selectSelectedWeekPeriodTasks);
 
   return (
     <Table
--- src/features/tasksTable/tasksTable.slice.ts
+++ src/features/tasksTable/tasksTable.slice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../../app/store';
 import { Task } from '../../interfaces/Task';
+import { format } from 'date-fns';
 
 const initialState: Task[] = [];
 
@@ -18,5 +19,18 @@ export const tasksTableSlice = createSlice({
 });
 
 export const { addedTask, loadedTasks } = tasksTableSlice.actions;
+
+export const selectSelectedWeekPeriodTasks = (state: RootState) => {
+  const selectedWeekStartOfWeek = state.weekSwitcher.startOfWeek;
+
+  return state.tasksTable.filter(
+    (task) => {
+      const formattedSelectedWeekStartOfWeek = format(new Date(selectedWeekStartOfWeek), 'dd.MM.yyyy');
+      const formattedTaskStartOfWeek = format(new Date(task.weekPeriod.startOfWeek), 'dd.MM.yyyy');
+
+      return formattedSelectedWeekStartOfWeek === formattedTaskStartOfWeek;
+    }
+  );
+};
 
 export default tasksTableSlice.reducer;
